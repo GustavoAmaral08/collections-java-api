f074e20f1cb6890df82cc271fef1ff778a198099
GustavoAmaral <amaral193tt@gmail.com>
2026-09-22 02:34:16 -0300

    Resolvendo desafio SomaDeNumeros
    Signed-off-by: GustavoAmaral <amaral193tt@gmail.com>

---
+++ src/list/OperacoesBasicas/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,78 @@
+package list.OperacoesBasicas.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoDePublicacao) {
+        livroList.add(new Livro(titulo, autor, anoDePublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        } else {
+        throw new RuntimeException("A lista está vazia!");
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervalosDeAnos(int anoInicial, int anoFinal) {
+
+        List<Livro> livrosPorIntervalosDeAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAnoDePublicacao() >= anoInicial && l.getAnoDePublicacao() <= anoFinal) {
+                    livrosPorIntervalosDeAnos.add(l);
+                }
+            }
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+
+        return livrosPorIntervalosDeAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+        return livroPorTitulo;
+    }
+
+    static void main() {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 4"));
+        System.out.println(catalogoLivros.pesquisarPorIntervalosDeAnos( 2020, 2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+}
---
+++ src/list/OperacoesBasicas/Pesquisa/SomaDeNumeros.java
@@ -0,0 +1,82 @@
+package list.OperacoesBasicas.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaDeNumeros {
+
+    private List<Integer> inteiroslist;
+
+    public SomaDeNumeros() {
+        this.inteiroslist = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        inteiroslist.add(new Integer(numero));
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for (Integer numero : inteiroslist) {
+            soma += numero;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = Integer.MIN_VALUE;
+
+        if (!inteiroslist.isEmpty()) {
+            for (Integer numero : inteiroslist) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+        } else {
+        throw new RuntimeException("A lista está vazia!");
+    }
+        return maiorNumero;
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        if(!inteiroslist.isEmpty()) {
+            for (Integer numero : inteiroslist) {
+                if(numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+        } else {
+        throw new RuntimeException("A lista está vazia!");
+    }
+        return menorNumero;
+    }
+
+    public void exibirNumeros(){
+        if (!inteiroslist.isEmpty()) {
+            System.out.println(this.inteiroslist);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    static void main() {
+
+        SomaDeNumeros somaNumeros = new SomaDeNumeros();
+
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+
+        System.out.println("Números adicionados:");
+        somaNumeros.exibirNumeros();
+
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+    }
+}
