fdda45e5a1b8d818022e675befd6cf54351ce9bb
GustavoAmaral <amaral193tt@gmail.com>
2026-09-22 03:10:04 -0300

    Incluindo a pasta Ordenacao
    Signed-off-by: GustavoAmaral <amaral193tt@gmail.com>

---
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,46 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    static void main() {
+
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("João", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Maria", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Luis", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Bia", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
